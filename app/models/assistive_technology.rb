a90bb97a68e0e4b5a849c4ab26372a32d5027ddc
Caleb Chao <calebchao@calebchao-macbookpro.roam.corp.google.com>
2015-06-11 16:02:05 -0700

    change all id input fields to drop downs. fix missing @platforms for new assistive technologies form

--- app/models/assistive_technology.rb
+++ app/models/assistive_technology.rb
@@ -1,4 +1,8 @@
 class AssistiveTechnology < ActiveRecord::Base
   belongs_to :platform, inverse_of: :assistive_technologies
   has_many :matrix_entries, inverse_of: :assistive_technology
+
+  def title_with_version
+    "#{title} #{version}"
+  end
 end
