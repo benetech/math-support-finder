307fc074534cd787b7ffe7e9776e1506b7c6b938
Caleb Chao <calebchao@calebchao-macbookpro.roam.corp.google.com>
2015-06-11 14:33:24 -0700

    change platform id text field to drop down list for editing assistive technology

--- app/models/platform.rb
+++ app/models/platform.rb
@@ -2,4 +2,8 @@ class Platform < ActiveRecord::Base
   has_many :assistive_technologies, inverse_of: :platform
   has_many :matrix_entries, inverse_of: :platform
   has_and_belongs_to_many :browser_readers
+
+  def title_with_version
+    "#{title} #{version}"
+  end
 end
