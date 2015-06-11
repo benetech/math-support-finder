eae9bca887901e57eae65575bfbdb0399ddb55a2
Sina Bahram <sina@sinabahram.com>
2015-06-11 12:21:32 -0400

    additional seed data and design folder

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,22 @@
-fPlatform.create([
+Platform.create([
 {title: "Android"},
 {title: "Chromebook"},
 {title: "iOS", version: "8"},
 {title: "Kindle"},
 {title: "Linux"},
-{title: "Mac"},
-{title: "Windows"},
+{title: "Mac OS X", version: "10.10"},
+{title: "Windows", version: "7"},
+{title: "Windows", version: "7/8/8.1"},
 {title: "Unknown"}])
 
+windows = Platform.find_by!(title: "Windows")
+AssistiveTechnology.create([
+{title: "Chromevox", platform: Platform.find_by!(title: "Chromebook")},
+{title: "JAWS", platform: windows},
+{title: "NVDA", platform: windows},
+{title: "Talkback", platform: Platform.find_by!(title: "Android")},
+{title: "Voiceover (desktop)", platform: Platform.find_by!(title: "Mac OS X")},
+{title: "Voiceover (mobile)", platform: Platform.find_by!(title: "iOS")},
+{title: "Windoweyes", platform: windows},
+{title: "Zoomtext", platform: windows},
+{title: "Unknown", platform: Platform.find_by!(title: "Unknown")}])
