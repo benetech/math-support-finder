59e4c66a4157e8387cb7fefc8b53be083ddc05e5
Lauren Staal <les6@rice.edu>
2015-06-11 12:21:57 -0700

    added homepage that says hello world

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
+  # home page
+  def index
+  end
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
