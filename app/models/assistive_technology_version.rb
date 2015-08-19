1404850ed0dbc5a5a43ee761954692b29e5fe61b
Christopher Reed <mail@seeread.info>
2015-08-19 15:28:29 -0400

    versioned entity improvements + cache buster for versions to versioned + unique version names per entity + order for versions + show correct versioned entity in version edit

--- app/models/assistive_technology_version.rb
+++ app/models/assistive_technology_version.rb
@@ -15,11 +15,14 @@
 #
 
 class AssistiveTechnologyVersion < ActiveRecord::Base
-  belongs_to :assistive_technology
+  belongs_to :assistive_technology, touch: true
   has_many :setups, dependent: :nullify
 
   validates_associated :assistive_technology
   validates_presence_of :assistive_technology, :version
+  validates :version, uniqueness: {scope: :assistive_technology_id}
+
+  default_scope { joins(:assistive_technology).order('LOWER(title), LOWER(version)') }
 
   def to_s
     [assistive_technology.to_s, version.to_s].join(" ")
