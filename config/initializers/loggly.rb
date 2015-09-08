6c55e9c807a5cb5ffdc543d6f2ab11632abc2fc1
Christopher Reed <mail@seeread.info>
2015-09-08 10:40:33 -0400

    loggly init; removes commented code around session store

--- config/initializers/loggly.rb
+++ config/initializers/loggly.rb
@@ -1,2 +1,2 @@
-#loggly = Logglier.new('https://logs-01.loggly.com/inputs/' + ENV['LOGGLY_TOKEN'] + '/tag/rails', threaded: true, format: :json)
-#Rails.logger.extend(ActiveSupport::Logger.broadcast(loggly))
+loggly = Logglier.new('https://logs-01.loggly.com/inputs/' + ENV['LOGGLY_TOKEN'] + '/tag/rails', threaded: false, format: :json) #attempting with false
+Rails.logger.extend(ActiveSupport::Logger.broadcast(loggly))
